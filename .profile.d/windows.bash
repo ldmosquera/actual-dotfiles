0effcb3e074d62ac5b1bccf198307d9d6843ff85
Leonardo Mosquera <ldmosquera@gmail.com>
2010-04-18 20:39:45 -0300

    Se agregan aliases para Windows.

---
+++ .profile.d/windows.bash
@@ -0,0 +1,12 @@
+alias start='cygstart'
+alias go='cygstart'
+
+alias pst='pstree -pAl'
+alias ns='netstat -a -p tcp -n -o'
+
+alias regasm='/c/Windows/Microsoft.NET/Framework/v2.0.50727/RegAsm.exe'
+alias regsvcs='/c/Windows/Microsoft.NET/Framework/v2.0.50727/RegSvcs.exe'
+alias tlbexp='/c/Archivos de programa/Microsoft Visual Studio 8/SDK/v2.0/Bin/TlbExp.exe'
+alias tlbimp='/c/Archivos de programa/Microsoft Visual Studio 8/SDK/v2.0/Bin/TlbImp.exe'
+
+alias vihosts='vim "$WINDIR"/system32/drivers/etc/hosts'
