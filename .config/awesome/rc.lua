05781bd9469c8dc884be66f2b51afabfec971494
Leonardo Mosquera <ldmosquera@gmail.com>
2012-06-17 23:52:42 -0300

    Se cambian tags con "s" y "d" en lugar de flechas.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -197,8 +197,8 @@ root.buttons(awful.util.table.join(
 
 -- {{{ Key bindings
 globalkeys = awful.util.table.join(
-    awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
-    awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
+    awful.key({ modkey,           }, "s",   awful.tag.viewprev       ),
+    awful.key({ modkey,           }, "d",  awful.tag.viewnext       ),
     awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
 
     awful.key({ modkey,           }, "j",
